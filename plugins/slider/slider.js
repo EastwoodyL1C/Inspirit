4f47eb58fed974503c3a9476284eb9e0beba8b8b
EastwoodyL1C <alex_eastwoody@yahoo.com>
2023-01-01 19:50:00 +0100

    v1.5
    final release

--- plugins/slider/slider.js
+++ plugins/slider/slider.js
@@ -1,6 +1,25 @@
-$('.owl-carousel').owlCarousel({
-    items:2,
-    lazyLoad:true,
-    loop:true,
-    margin:10
-});
+function carouselFn(){
+    if ($(window).width() < 500){
+       $(".owl-carousel").owlCarousel({
+   
+        items:1,
+        margin:10,
+        loop:true,
+        });
+     }
+     else{
+      $(".owl-carousel").owlCarousel({
+   
+       items:2,
+       margin:20,
+       loop:true,
+      });
+     }
+    };
+   
+    $(document).ready(function() {
+        carouselFn();
+   });
+   $(window).resize(function(){
+        carouselFn();     
+    });
